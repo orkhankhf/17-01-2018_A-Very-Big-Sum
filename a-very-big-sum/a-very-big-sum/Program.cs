3e53dfed32a82366ecf0fc0d10f8263e8c2e08dd
Orkhan Farajov <orkhankhf@code.edu.az>
2018-01-18 21:40:51 +0400

    Calc big integers

---
+++ a-very-big-sum/a-very-big-sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace a_very_big_sum
+{
+    class Program
+    {
+        static long aVeryBigSum(int n, long[] ar)
+        {
+            return ar.Sum();
+            /*
+            
+            OR THIS
+
+            long sum = 0;
+            foreach (var i in ar)
+            {
+                sum += i;
+            }
+            return sum;*/
+        }
+        static void Main(string[] args)
+        {
+            /*
+             input this;
+             5
+             1000000001 1000000002 1000000003 1000000004 1000000005
+            */
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+            long result = aVeryBigSum(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
